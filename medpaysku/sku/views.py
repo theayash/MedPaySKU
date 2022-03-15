de8f05ddfe11fe9b3115ffabbd5de69eea7cdf04
theayash <ayashmishra@gmail.com>
2022-03-15 09:32:30 +0530

    Created API skeletons and swagger documentations

--- medpaysku/sku/views.py
+++ medpaysku/sku/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+"""
+Get the transaction details by transaction id
+"""
+
+
+class TransactionDetails(APIView):
+    def get(self, request, transaction_id, format=None):
+        return Response(transaction_id)
+
+
+"""
+Get transaction summary by SKU
+"""
+
+
+class TransactionSummaryBySKU(APIView):
+    def get(self, request, number_of_days, format=None):
+        return Response(number_of_days)
+
+
+"""
+Get transaction summary by SKU category
+"""
+
+
+class TransactionSummaryBySKUCategory(APIView):
+    def get(self, request, number_of_days, format=None):
+        return Response(number_of_days)
+
