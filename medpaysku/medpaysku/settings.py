e8f93e6341109b0ab08ad7adc950896bec2bd39a
theayash <ayashmishra@gmail.com>
2022-03-16 12:56:50 +0530

    Added Docker File and set Debug=True

--- medpaysku/medpaysku/settings.py
+++ medpaysku/medpaysku/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z(%z)ftke+xct)=+*zjp24w@ixa&uhw%1_3ut(0^ec#%lnpb@+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
